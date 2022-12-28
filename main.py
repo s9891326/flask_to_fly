d61e6d49fa2e07f1695ed4ef681bef0ef18679bd
Eddy <eddy15201@gmail.com>
2022-12-28 15:41:18 +0800

    test

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
     return f"hello world: {os.getenv('DB_HOST')}, {os.getenv('a')}"
 
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', 8080)
+if __name__ == "__main__":
+    app.run("0.0.0.0", 8080)
